734163a957d5ec16d2f47f8e5801bd14e595fc03
nifle3 <nifle3123@gmail.com>
2025-09-19 11:30:00 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A sudoku clojurescript flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ...}:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+    in {
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          clojure
+          nodejs_24
+          leiningen
+          clojure-lsp
+          cljfmt
+        ];
+
+        shellHook = ''
+          echo "hello world";
+        '';
+      };
+    }
+  );
+}
